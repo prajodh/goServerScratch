8d5b0f30a904e30be6a08a7fad5fee332f5bdd46
prajodh <prajodhpragaths@gmail.com>
2024-08-05 22:15:46 -0500

    created a web server using fileserver to handle and server a html page and an image

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import ("net/http"
+"fmt")
+
 
 func main(){
-	
+	serverMux := http.NewServeMux()
+	server := http.Server{Handler: serverMux}
+	server.Addr = "localhost:8080"
+	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+	serverMux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	err := server.ListenAndServe()
+	if err!=nil{
+		fmt.Println(err)
+	}
 }
