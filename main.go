09b2255375e86d2e068c6797d76f30435157cbc4
prajodh <prajodhpragaths@gmail.com>
2024-08-07 22:12:59 -0500

    added validate chirpy endpoint

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
+	// "errors"
 )
 
 
@@ -37,6 +39,46 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, R *http.Request){
 	w.Write([]byte(val))
 }
 
+func writeResponseValidateChrip(v bool, errs string) []byte {
+		type returnBodyValidateChrip struct{
+			Valid bool
+			Errors string
+		}
+		returnbody := returnBodyValidateChrip{Valid: v, Errors: errs}
+		res, err := json.Marshal(returnbody)
+		if err != nil{
+			fmt.Println(err)
+		}
+		return res
+		
+}
+func validateChirpHandler(w http.ResponseWriter, r *http.Request){
+	header := w.Header()
+	type body struct{
+		Body string
+	}
+	decoder := json.NewDecoder(r.Body)
+	b:=body{}
+	err := decoder.Decode(&b)
+	if err != nil{
+		res := writeResponseValidateChrip(false, err.Error())
+		header["Content-Type"] = []string{"application/json; charset=utf-8"}
+		w.WriteHeader(500)
+		w.Write(res)
+	}
+	if len(b.Body) > 140{
+		res := writeResponseValidateChrip(false, "chrip is too long")	
+		header["Content-Type"] = []string{"application/json; charset=utf-8"}
+		w.WriteHeader(400)
+		w.Write(res)
+		} else{
+		res := writeResponseValidateChrip(true, "")		
+		header["Content-Type"] = []string{"application/json; charset=utf-8"}
+		w.WriteHeader(200)
+		w.Write(res)
+	}
+}
+
 
 func main(){
 	apiconfig := &apiConfig{}
@@ -54,7 +96,7 @@ func main(){
 	})
 	serverMux.HandleFunc("GET /admin/metrics",apiconfig.metricsHandler)
 	serverMux.HandleFunc("GET /admin/reset",apiconfig.resetHandler)
-
+	serverMux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 	err := server.ListenAndServe()
 	if err!=nil{
 		fmt.Println(err)
